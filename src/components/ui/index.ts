d327a5a96843b49f85585e5feee2ffa868a24be2
Dmitry Soboliev <19leva89@gmail.com>
2025-04-28 14:53:47 +0300

    add shadcn

---
+++ src/components/ui/index.ts
@@ -0,0 +1,91 @@
+export {
+	AlertDialog,
+	AlertDialogTrigger,
+	AlertDialogContent,
+	AlertDialogHeader,
+	AlertDialogFooter,
+	AlertDialogTitle,
+	AlertDialogDescription,
+	AlertDialogAction,
+	AlertDialogCancel,
+} from './alert-dialog'
+export { Avatar, AvatarFallback, AvatarImage } from './avatar'
+export { Button, buttonVariants } from './button'
+export { Calendar } from './calendar'
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card'
+export { ChartContainer, ChartTooltip, ChartTooltipContent } from './chart'
+export type { ChartConfig } from './chart'
+export { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from './command'
+export {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from './dialog'
+export {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from './dropdown-menu'
+export { Input } from './input'
+export { Label } from './label'
+export {
+	Pagination,
+	PaginationContent,
+	PaginationEllipsis,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+} from './pagination'
+export { Popover, PopoverTrigger, PopoverContent } from './popover'
+export { ScrollArea, ScrollBar } from './scroll-area'
+export { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './select'
+export { Separator } from './separator'
+export {
+	Sheet,
+	SheetClose,
+	SheetContent,
+	SheetDescription,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from './sheet'
+export {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarHeader,
+	SidebarInset,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarProvider,
+	SidebarTrigger,
+	useSidebar,
+} from './sidebar'
+export { Skeleton } from './skeleton'
+export {
+	Table,
+	TableHeader,
+	TableBody,
+	TableFooter,
+	TableHead,
+	TableRow,
+	TableCell,
+	TableCaption,
+} from './table'
+export { Tabs, TabsList, TabsTrigger, TabsContent } from './tabs'
+export { Textarea } from './textarea'
+export { Toaster } from './sonner'
+export { Toggle, toggleVariants } from './toggle'
+export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './tooltip'
